ad9183a9f179c87d40fc265d7264a29b107bfb2a
lblack <wanglongqi77@gmail.com>
2021-06-13 15:17:21 -0400

    added some links and docs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -56,6 +56,9 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../public/transact.html"));
   });
 
+  app.get("/condodocs", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/condoDocs.pdf"));
+  });
   // Load example page and pass in an example by id
   // app.get("/example/:id", function (req, res) {
   //   db.Example.findOne({ where: { id: req.params.id } }).then(function (
