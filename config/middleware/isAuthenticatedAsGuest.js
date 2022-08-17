577221f5977383a54aa0ac6ba4f9306e3b18a008
lblack <wanglongqi77@gmail.com>
2022-08-17 17:03:20 -0400

    fixed redirect bug

--- config/middleware/isAuthenticatedAsGuest.js
+++ config/middleware/isAuthenticatedAsGuest.js
@@ -2,6 +2,8 @@ module.exports = function isAuthenticatedAsGuest(req, res, next) {
   if (req.user) {
     return next();
   }
+  //added this next line to try to redirect to previous page after logged in
+  req.session.returnTo = req.originalUrl;
   return res.render("login", {
     message: "You must login to view the list of residents."
   });
